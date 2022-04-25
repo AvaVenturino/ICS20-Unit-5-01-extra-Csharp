8506306e39d15c277be8ed60ddfe1e1f03849fe6
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-04-25 22:05:13 +0000

    deleted whitespaces

--- main.cs
+++ main.cs
@@ -10,17 +10,13 @@ class Program
     public static void Main(string[] args)
   {
     Random random = new Random();
-
     int returnValue = random.Next(1, 6);
     int Guess = 0;
-
     Console.WriteLine("Think of a number between 1-6. Can you guess what it is?");
-
     if (Guess != returnValue)
     {
       Guess = Convert.ToInt32(Console.Read());
       Console.WriteLine("Try again! The answer was " + returnValue);
-
     }
     if (Guess == returnValue)
     {
