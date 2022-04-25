8a6d4bda59ce03e892542243747ab9061f2ef249
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-04-25 22:10:39 +0000

    fixed whitespace

--- main.cs
+++ main.cs
@@ -8,21 +8,21 @@ using System;
 class Program
 {
     public static void Main(string[] args)
-  {
-    Random random = new Random();
-    int returnValue = random.Next(1, 6);
-    int Guess = 0;
-    Console.WriteLine("Think of a number between 1-6. Can you guess what it is?");
-    if (Guess != returnValue)
     {
-      Guess = Convert.ToInt32(Console.Read());
-      Console.WriteLine("Try again! The answer was " + returnValue);
+        Random random = new Random();
+        int returnValue = random.Next(1, 6);
+        int Guess = 0;
+        Console.WriteLine("Think of a number between 1-6. Can you guess what it is?");
+        if (Guess != returnValue)
+        {
+            Guess = Convert.ToInt32(Console.Read());
+            Console.WriteLine("Try again! The answer was " + returnValue);
+        }
+        if (Guess == returnValue)
+        {
+            Console.WriteLine("Well done! The answer was " + returnValue);
+            Console.ReadLine();
+        }
     }
-    if (Guess == returnValue)
-    {
-        Console.WriteLine("Well done! The answer was " + returnValue);
-        Console.ReadLine();
-    }
-  }
 }
   
